5fe47837835163ef9e72ab416e3b824e8d39ad2d
Marcus <vini.mark2002@gmail.com>
2022-05-08 11:27:57 -0300

    Stage 4 TODO:  - Consertar a falta do envio de imagem por e-mail

---
+++ aulas/mobile/src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    TouchableOpacityProps,
+    ActivityIndicator
+} from 'react-native';
+import { theme } from '../../theme';
+
+import { styles } from './styles';
+
+interface Props extends TouchableOpacityProps {
+    isLoading: boolean;
+}
+
+export function Button({ isLoading, ...rest }: Props) {
+    return (
+        <TouchableOpacity
+            style={styles.container}
+            {...rest}
+        >
+            {
+                isLoading
+                ?
+                <ActivityIndicator
+                    color={theme.colors.text_on_brand_color}
+                />
+                :
+                <Text style={styles.title}>
+                    Eviar Feedback
+                </Text>
+            }
+        </TouchableOpacity>
+    );
+}
